5db2fadcb01b2163ca3b0d89deec70d829023c2b
JUNU <bokjunwoo@gmail.com>
2023-11-11 03:53:29 +0900

    feat : 사용자로부터 날짜 입력받는 UserInputDate 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import InputView from './InputView.js';
 import OutputView from './OutputView.js';
+import VisitDate from './VisitDate.js';
 
 class App {
+  #visitDate;
+
   async run() {
     OutputView.printIntroduction();
+
+    await this.UserInputDate();
+  }
+
+  async UserInputDate() {
+    while (true) {
+      try {
+        const input = await InputView.inputDate();
+        this.#visitDate = new VisitDate(input);
+        break;
+      } catch (error) {
+        OutputView.printErrorMessage(error.message);
+      }
+    }
   }
 }
 
