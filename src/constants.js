5b5ea429fa0b1a9129925fa389315fd146063df8
JUNU <bokjunwoo@gmail.com>
2023-11-11 01:49:29 +0900

    add : VisitDate 클래스에 대한 에러 메시지 상수 추가

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGE = {
+  NOT_NUMERIC: '[ERROR] 날짜는 숫자여야 합니다. 다시 입력해 주세요.',
+  NOT_INTEGER: '[ERROR] 날짜는 정수여야 합니다. 다시 입력해 주세요.',
+  INVALID_DATE: '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.',
+};
