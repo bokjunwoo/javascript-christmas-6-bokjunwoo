562227273aa396a4a2b5e56e444297c6c5ef5b22
JUNU <bokjunwoo@gmail.com>
2023-11-13 01:15:10 +0900

    feat: VisitDate 클래스에 날짜 관련 메소드 구현
    - isChristmasDiscountDay: 방문 날짜가 크리스마스 할인 기간 내인지 확인하는 메소드 추가
    - isWeekend: 방문 날짜가 주말인지 확인하는 메소드 추가
    - isSpecialDiscountDay: 방문 날짜가 특별 할인 날인지 확인하는 메소드 추가 (일요일 또는 크리스마스)

--- src/VisitDate.js
+++ src/VisitDate.js
@@ -1,16 +1,36 @@
 import { isValidVisitDate } from './utils.js';
 
 class VisitDate {
-  #date
+  #date;
 
   constructor(date) {
-    this.#validate(date)
+    this.#validate(date);
     this.#date = date;
   }
 
   #validate(date) {
-    isValidVisitDate(date)
+    isValidVisitDate(date);
+  }
+
+  isChristmasDiscountDay() {
+    if (this.#date >= 1 && this.#date <= 25) {
+      return true;
+    }
+    return false;
+  }
+
+  isWeekend() {
+    const dayOfWeek = new Date(`2023-12-${this.#date}`).getDay();
+    return dayOfWeek === 5 || dayOfWeek === 6;
+  }
+
+  isSpecialDiscountDay() {
+    const dayOfWeek = new Date(`2023-12-${this.#date}`).getDay();
+    const isSunday = dayOfWeek === 0;
+    const isChristmas = this.#date === 25;
+
+    return isSunday || isChristmas;
   }
 }
 
-export default VisitDate
+export default VisitDate;
