97a36d867ff0fe0785e81f1886a7a33b38afa0f5
JUNU <bokjunwoo@gmail.com>
2023-11-12 23:09:27 +0900

    feat: Order 클래스에 addMenuItem 메서드 추가
    - 주문 수량 및 아이템 목록을 업데이트하고, 유효성 검사 수행
    - 메뉴 정보는 Menu 클래스의 menuInfo 메서드를 활용하여 가져옴
    - 주문 수량은 private 필드로 저장되며, 제한을 초과하지 않도록 유효성 체크함

--- src/Order.js
+++ src/Order.js
@@ -1,10 +1,21 @@
 import { checkOrderQuantityLimit, validateMenuQuantity } from './utils.js';
 
 class Order {
+  #orderItems;
   #orderQuantity;
 
   constructor() {
+    this.#orderItems = [];
     this.#orderQuantity = 0;
+  }
+
+  addMenuItem(menu, quantity) {
+    this.validateAndAdd(quantity);
+
+    const { name, price, type } = menu.menuInfo();
+
+    this.#orderItems.push({ name, quantity, price, type });
+    this.#orderQuantity += quantity;
   }
 
   validateAndAdd(quantity) {
