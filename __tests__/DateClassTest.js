9e3d4d1159ec1153f2bdf964422baa0eb17ca316
JUNU <bokjunwoo@gmail.com>
2023-11-13 01:10:40 +0900

    test: VisitDate 날짜 관련 메소드 테스트 추가

--- __tests__/DateClassTest.js
+++ __tests__/DateClassTest.js
@@ -24,4 +24,36 @@ describe('VisitDate 클래스 테스트', () => {
       new VisitDate('0');
     }).toThrow('[ERROR]');
   });
+
+  describe('VisitDate의 날짜 관련 메소드', () => {
+    test('1일부터 25일까지 isChristmasDiscountDay()가 true를 반환', () => {
+      const visitDate = new VisitDate(1);
+      expect(visitDate.isChristmasDiscountDay()).toBe(true);
+    });
+
+    test('26일부터 isChristmasDiscountDay()가 false를 반환', () => {
+      const visitDate = new VisitDate(26);
+      expect(visitDate.isChristmasDiscountDay()).toBe(false);
+    });
+
+    test('주중(일 ~ 목)에 isWeekend()는 false를 반환', () => {
+      const visitDate = new VisitDate(6);
+      expect(visitDate.isWeekend()).toBe(false);
+    });
+
+    test('주말(금 ~ 토)에 isWeekend()는 true를 반환', () => {
+      const visitDate = new VisitDate(8);
+      expect(visitDate.isWeekend()).toBe(true);
+    });
+
+    test('25일인 경우 isSpecialDiscountDay()가 true를 반환', () => {
+      const visitDate = new VisitDate(25);
+      expect(visitDate.isSpecialDiscountDay()).toBe(true);
+    });
+
+    test('11일인 경우 isSpecialDiscountDay()가 false를 반환', () => {
+      const visitDate = new VisitDate(11);
+      expect(visitDate.isSpecialDiscountDay()).toBe(false);
+    });
+  });
 });
