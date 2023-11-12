82668064ebc93a56ffe12dac404e89ea2acbbcc4
JUNU <bokjunwoo@gmail.com>
2023-11-13 00:00:20 +0900

    test: Order 클래스의 calculateTotal 메서드에 대한 단위 테스트 추가

---
+++ __tests__/OrderTest.js
@@ -0,0 +1,18 @@
+import Menu from '../src/Menu';
+import Order from '../src/Order';
+
+describe('Order 클래스', () => {
+  test('calculateTotal() 메소드는 올바른 총 가격을 계산', () => {
+    const order = new Order();
+
+    const menu1 = new Menu('초코케이크');
+    const menu1Quantity = 2;
+    const menu2 = new Menu('레드와인');
+    const menu2Quantity = 1;
+
+    order.addMenuItem(menu1, menu1Quantity);
+    order.addMenuItem(menu2, menu2Quantity);
+
+    expect(order.calculateTotal()).toBe(90000);
+  });
+});
