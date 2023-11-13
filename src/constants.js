7cce65806983c3cf1717c3f67d2e57bb184f3cb4
JUNU <bokjunwoo@gmail.com>
2023-11-14 03:04:30 +0900

    fix: ApplicationTest 테스트 통과를 위한 validate 메세지 수정

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,8 @@
 export const ERROR_MESSAGE = {
-  NOT_NUMERIC: '[ERROR] 날짜는 숫자여야 합니다. 다시 입력해 주세요.',
+  NOT_NUMERIC: '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.',
   NOT_INTEGER: '[ERROR] 날짜는 정수여야 합니다. 다시 입력해 주세요.',
-  INVALID_DATE: '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.',
+  INVALID_DATE:
+    '[ERROR] 날짜는 1 ~ 31 사이의 숫자를 입력해야 합니다. 다시 입력해 주세요.',
 };
 
 export const RESTAURANT_EVENT_PLANNER_MESSAGE = `안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.
--- src/utils.js
+++ src/utils.js
@@ -21,19 +21,19 @@ export const isValidVisitDate = (date) => {
 
 export const validateMenuName = (name) => {
   if (!menuData.find((menu) => menu.name === name)) {
-    throw new Error('[ERROR] 해당 메뉴를 찾을 수 없습니다.');
+    throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
   }
 };
 
 export const validateMenuQuantity = (quantity) => {
   const numericQuantity = Number(quantity);
 
-  if (quantity === undefined) {
-    throw new Error('[ERROR] 주문하신 메뉴의 수량이 입력되지 않았습니다.');
+  if (isNaN(numericQuantity)) {
+    throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
   }
 
-  if (isNaN(numericQuantity)) {
-    throw new Error('[ERROR] 메뉴의 수량은 숫자여야 합니다.');
+  if (quantity === undefined) {
+    throw new Error('[ERROR] 주문하신 메뉴의 수량이 입력되지 않았습니다.');
   }
 
   if (!Number.isInteger(numericQuantity)) {
