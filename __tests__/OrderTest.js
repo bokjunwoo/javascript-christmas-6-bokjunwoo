88dd7a80461e4081cc577e2953da746a0bb9d3e4
JUNU <bokjunwoo@gmail.com>
2023-11-15 03:44:28 +0900

    test: calculateTotalAmount 메소드 테스트 코드 수정

--- __tests__/OrderTest.js
+++ __tests__/OrderTest.js
@@ -2,17 +2,35 @@ import Menu from '../src/Menu';
 import Order from '../src/Order';
 
 describe('Order 클래스', () => {
-  test('calculateTotalPrice() 메소드는 올바른 총 가격을 계산', () => {
-    const order = new Order();
+  let order;
 
-    const menu1 = new Menu('초코케이크');
-    const menu1Quantity = 2;
-    const menu2 = new Menu('레드와인');
-    const menu2Quantity = 1;
+  beforeEach(() => {
+    order = new Order();
+  });
 
-    order.addMenuItem(menu1, menu1Quantity);
-    order.addMenuItem(menu2, menu2Quantity);
+  describe('calculateTotalAmount 메소드', () => {
+    test('올바른 총 가격을 계산', () => {
+      const menu1 = new Menu('초코케이크');
+      const menu1Quantity = 2;
+      const menu2 = new Menu('레드와인');
+      const menu2Quantity = 1;
 
-    expect(order.calculateTotalPrice()).toBe(90000);
+      order.addMenuItem(menu1, menu1Quantity);
+      order.addMenuItem(menu2, menu2Quantity);
+
+      expect(order.calculateTotalAmount()).toBe(90000);
+    });
+
+    test('올바른 계산이 아닌 경우', () => {
+      const menu1 = new Menu('초코케이크');
+      const menu1Quantity = 2;
+      const menu2 = new Menu('레드와인');
+      const menu2Quantity = 1;
+
+      order.addMenuItem(menu1, menu1Quantity);
+      order.addMenuItem(menu2, menu2Quantity);
+
+      expect(order.calculateTotalAmount()).not.toBe(100000);
+    });
   });
 });
